4231530d91e415dca7d8990e0ab86b14f2f5e73d
TiokengSamuel <samueledward40@gmail.com>
2020-10-24 13:31:25 +0100

    register

--- course/src/components/Auth/Register.js
+++ course/src/components/Auth/Register.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Grid, Form, Segment, Button} from 'semantic-ui-react';
+import {Grid, Form, Segment, Button, Header, Message, Icon} from 'semantic-ui-react';
 
 class Register extends React.Component {
     render() {
