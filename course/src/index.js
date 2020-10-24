7ca5746d3caabfebf22cb5aa583dd0c1dfcb253d
TiokengSamuel <samueledward40@gmail.com>
2020-10-24 09:18:39 +0100

    index

--- course/src/index.js
+++ course/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-
+    import  { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
