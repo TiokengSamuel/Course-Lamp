483c942ee41c06e8a92db462a9113f86a1fee69e
TiokengSamuel <samueledward40@gmail.com>
2020-10-25 16:12:39 +0100

    redux connect

--- course/src/index.js
+++ course/src/index.js
@@ -9,7 +9,7 @@ import 'semantic-ui-css/semantic.min.css';
 import firebase from './Firebase'
 
 import { createStore } from 'redux';
-import { Provider } from 'react-redux'
+import { Provider, connect } from 'react-redux'
 import { composeWithDevTools, composewithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers';
 
