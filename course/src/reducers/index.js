4ff2bd6d723f8fdb4f276b5cd35ee36f6461ccda
TiokengSamuel <samueledward40@gmail.com>
2020-10-25 16:01:40 +0100

    ind

--- course/src/reducers/index.js
+++ course/src/reducers/index.js
@@ -2,5 +2,10 @@ import * as actionTypes from '../actions/types'
 
 
 const user_reducer = (state, action) => {
-    
+    switch (action.type) {
+        case actionTypes.SET_USER;
+        return {
+            curr
+        }
+    }
 }
