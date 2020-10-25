09c74c6411db2dae23bd8377e04a18b03ffa92af
TiokengSamuel <samueledward40@gmail.com>
2020-10-25 21:12:04 +0100

    app

--- course/src/components/App.js
+++ course/src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Grid } from 'semantic-ui-react'
+import ColorPanel from './ColorPanel/ColorPanel';
+import Messages from './Messages/Messages';
+import MetaPanel from './MetaPanel/MetaPanel';
+import SidePanel from './SidePanel/SidePanel';
 
 export const App = () => (
   <Grid>
