4cfb937489085bf2ebc39d6415df9a6dcaa23695
TiokengSamuel <samueledward40@gmail.com>
2020-10-25 22:16:15 +0100

    sid

--- course/src/components/SidePanel/SidePanel.js
+++ course/src/components/SidePanel/SidePanel.js
@@ -9,7 +9,7 @@ export default class SidePanel extends Component {
                 inverted
                 fixed="left"
                 vertical
-                style={{ background: '#4c3c4c', fontSize: '1.2rem'}}
+                style={{ background: '#4c3c4c', fontSize: '1.2rem' }}
             >
 
             </Menu>
