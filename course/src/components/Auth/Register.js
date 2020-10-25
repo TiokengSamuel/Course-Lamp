7ba859557a56cd3ab9bb193837d7b778c2b2219d
TiokengSamuel <samueledward40@gmail.com>
2020-10-25 08:19:14 +0100

    register

--- course/src/components/Auth/Register.js
+++ course/src/components/Auth/Register.js
@@ -72,7 +72,7 @@ class Register extends React.Component {
                 console.log(createdUser)
                 createdUser.user.updateProfile({
                     displayName: this.state.username,
-                    photoURL: `http://gravatar.com/avatar/${}`
+                    photoURL: `http://gravatar.com/avatar/${createdUser.user.email}`
                 })
                 //this.setState({ loading: false })
             }) 
