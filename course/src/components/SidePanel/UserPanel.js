992b7bce45f26b5b93c699a2058f95bdf1b442cf
TiokengSamuel <samueledward40@gmail.com>
2020-10-25 22:21:54 +0100

    user

--- course/src/components/SidePanel/UserPanel.js
+++ course/src/components/SidePanel/UserPanel.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import { Grid } from 'semantic-ui-react'
 
 export default class UserPanel extends Component {
     render() {
         return (
-            
+            <Grid>
+                
+            </Grid>
         )
     }
 }
