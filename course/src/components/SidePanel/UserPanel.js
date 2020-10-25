a6f2842bc943b72e9b987d0c02e7772726a7cc26
TiokengSamuel <samueledward40@gmail.com>
2020-10-25 22:38:34 +0100

    user

--- course/src/components/SidePanel/UserPanel.js
+++ course/src/components/SidePanel/UserPanel.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Grid } from 'semantic-ui-react'
+import { Grid, Header } from 'semantic-ui-react'
 
 export default class UserPanel extends Component {
     render() {
@@ -7,7 +7,9 @@ export default class UserPanel extends Component {
             <Grid style = {{ background: '#4c3c4c'}}>
                 <Grid.Column>
                     <Grid.Row style={{ padding: '1.2em', margin:0 }}>
-
+                        <Header>
+                            
+                        </Header>
                     </Grid.Row>
                 </Grid.Column>
             </Grid>
