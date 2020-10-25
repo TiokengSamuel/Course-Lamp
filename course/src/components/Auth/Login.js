04fb1d7523387b227ca03becab53f51a884c45d9
TiokengSamuel <samueledward40@gmail.com>
2020-10-25 14:21:09 +0100

    violet

--- course/src/components/Auth/Login.js
+++ course/src/components/Auth/Login.js
@@ -57,7 +57,7 @@ class Login extends React.Component {
             <Grid textAlign="center" verticalAlign="middle" className="app">
                 <Grid.Column style={{ maxWidth: 450}}>
                     <Header as="h1" icon color="violet" textAlign="center">
-                        <Icon name="code branch" color="orange"/>
+                        <Icon name="code branch" color="violet"/>
                         Register for CourseLamp
                     </Header>
                     <Form onSubmit={this.handleSubmit} size="large">
