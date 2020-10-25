d0f571491039463a2c76f526df701f02aee518f6
TiokengSamuel <samueledward40@gmail.com>
2020-10-25 22:21:25 +0100

    grid

--- course/src/components/SidePanel/SidePanel.js
+++ course/src/components/SidePanel/SidePanel.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Menu } from 'semantic-ui-react'
+import UserPanel from './UserPanel'
 
 export default class SidePanel extends Component {
     render() {
