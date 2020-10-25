21cc871ca5758852e4bbaf7dd465fe5b70b2f3d7
TiokengSamuel <samueledward40@gmail.com>
2020-10-25 16:41:13 +0100

    spin

--- course/src/Spinner.js
+++ course/src/Spinner.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Loader } from 'semantic-ui-react'
 
 const Spinner = () => (
-    <Loader size="huge" content={"Preparing Chat"}/>
+    <Loader size="huge" content={"Preparing Chat..."}/>
 )
 
 export default Spinner;
