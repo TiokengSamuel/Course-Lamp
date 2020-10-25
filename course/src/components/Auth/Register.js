898aaf90f1b0b7ce471268ddd79e7641a483cbc9
TiokengSamuel <samueledward40@gmail.com>
2020-10-25 01:05:54 +0100

    register

--- course/src/components/Auth/Register.js
+++ course/src/components/Auth/Register.js
@@ -131,10 +131,10 @@ class Register extends React.Component {
                             </Button>
                         </Segment>
                     </Form>
-                    {errors.length > 0 && (
+                    {this.state.errors.length > 0 && (
                         <Message error>
                             <h3>Error</h3>
-                            {this.displayErrors(errors)}
+                            {this.displayErrors(this.state.errors)}
                         </Message>
                     )}
                     <Message>Already a user? <Link to="/login">Login</Link></Message>
