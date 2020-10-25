587e8a859225bf805cf0ff67ce154320478af39f
TiokengSamuel <samueledward40@gmail.com>
2020-10-25 22:14:59 +0100

    side

--- course/src/components/SidePanel/SidePanel.js
+++ course/src/components/SidePanel/SidePanel.js
@@ -4,8 +4,14 @@ import { Menu } from 'semantic-ui-react'
 export default class SidePanel extends Component {
     render() {
         return (
-            <Menu>
-                
+            <Menu
+                size="large"
+                inverted
+                fixed="left"
+                vertical
+                style={{ background}}
+            >
+
             </Menu>
         )
     }
