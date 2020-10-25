5a432adf1f20b6dd7cda11f0f1b3d07e4f483d85
TiokengSamuel <samueledward40@gmail.com>
2020-10-25 22:51:10 +0100

    user

--- course/src/components/SidePanel/UserPanel.js
+++ course/src/components/SidePanel/UserPanel.js
@@ -6,8 +6,8 @@ export default class UserPanel extends Component {
         return (
             <Grid style = {{ background: '#4c3c4c'}}>
                 <Grid.Column>
-                    <Grid.Row style={{ padding: '1.2em', margin:0 }}>
-                        <Header inverted floated="left" as="h3">
+                    <Grid.Row style={{ padding: '1em', margin:0 }}>
+                        <Header inverted floated="left" as="h2">
                             <Icon name="lightbulb outline"/>
                             <Header.Content>Course Lamp</Header.Content>
                         </Header>
