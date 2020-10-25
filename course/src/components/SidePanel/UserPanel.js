83224999ac105533de1867ee46486d72d4c3601f
TiokengSamuel <samueledward40@gmail.com>
2020-10-25 23:12:06 +0100

    user

--- course/src/components/SidePanel/UserPanel.js
+++ course/src/components/SidePanel/UserPanel.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import { Dropdown, Grid, Header, Icon } from 'semantic-ui-react'
 
 export default class UserPanel extends Component {
+    dropdownOptions = () => [
+        {}
+    ]
+
     render() {
         return (
             <Grid style = {{ background: '#4c3c4c'}}>
