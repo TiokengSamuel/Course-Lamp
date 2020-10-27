ff7e7bfe479f48c50e9360fc1f7f3bf82146b815
TiokengSamuel <samueledward40@gmail.com>
2020-10-27 12:11:30 +0100

    chanekk

--- course/src/components/SidePanel/Channels.js
+++ course/src/components/SidePanel/Channels.js
@@ -11,7 +11,7 @@ export default class Channels extends Component {
         const { channels } = this.state;
 
         return (
-            <Menu.Menu>
+            <Menu.Menu style={{ paddingBottom }}>
                 <Menu.Item>
                     <span>
                         <Icon name="exchange"/> CHANNELS
