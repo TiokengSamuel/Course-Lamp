8b842261026914c70e41a5cb130e48f5ac2aa4b9
TiokengSamuel <samueledward40@gmail.com>
2020-10-27 11:49:34 +0100

    channel

--- course/src/components/SidePanel/Channel.js
+++ course/src/components/SidePanel/Channel.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Icon, Menu } from 'semantic-ui-react'
 
 export default class Channel extends Component {
     render() {
         return (
             <Menu.menu>
-                
+                <Menu.Item>
+                    <span>
+                        <Icon/>
+                    </span>
+                </Menu.Item>
             </Menu.menu>
         )
     }
