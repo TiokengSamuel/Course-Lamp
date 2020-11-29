9f11174c38df2945bb52a0b5df11ea7b93834790
TiokengSamuel <samueledward40@gmail.com>
2020-11-29 21:25:36 +0100

    debuging

--- course/src/components/SidePanel/Channels.js
+++ course/src/components/SidePanel/Channels.js
@@ -43,7 +43,6 @@ export default class Channels extends Component {
     handleSubmit = event => {
         event.preventDefault();
         if (this.isFormValid(this.state)) {
-            console.log('Channel added')
             this.addChannel();
         }
     }
