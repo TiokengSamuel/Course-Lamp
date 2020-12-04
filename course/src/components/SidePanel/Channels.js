7d392e8315d21034281f4ba56fe9809069445d29
TiokengSamuel <samueledward40@gmail.com>
2020-12-04 10:55:51 +0100

    actions

--- course/src/components/SidePanel/Channels.js
+++ course/src/components/SidePanel/Channels.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Icon, Menu, Modal, Form, Input, Button } from 'semantic-ui-react'
 import { connect } from 'react-redux';
+import { setCurrentChannel } from '../../actions'
 import firebase from '../../Firebase';
 
 export default class Channels extends Component {
