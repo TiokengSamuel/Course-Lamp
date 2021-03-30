fceeb6350ee2addc193deb0d269650f8f3c99faa
TiokengSamuel <samueledward40@gmail.com>
2021-03-30 11:59:48 +0100

    finished ui

--- course/src/components/Messages/MessagesHeader.js
+++ course/src/components/Messages/MessagesHeader.js
@@ -6,10 +6,22 @@ class MessagesHeader extends React.component {
     render() {
         return(
             <Segment clearing>
+                {/* Channel title */}
                 <Header fluid = "true" as="h2" floated="left" style={{ marginBottom: 0 }}>
                     <span>
                     Channel
                     <Icon name={"star outline"} color="black"/>
+                    </span>
+                </Header>
+
+                {/* Channel Search Input */}
+                <Header floated="right">
+                    <Input
+                        size="mini"
+                        icon="search"
+                        name="searchTerm"
+                        placeholder="Search Messages"
+                    />
                 </Header>
             </Segment>
         )
